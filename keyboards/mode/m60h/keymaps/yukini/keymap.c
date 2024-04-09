1b31c4e40331a2b939b0674d3f7637ccc478c9b5
Yukini <y.mukuna.0724@gmail.com>
2024-04-09 19:38:36 +0900

    add: mode tempo

--- keyboards/mode/m60h/keymaps/yukini/keymap.c
+++ keyboards/mode/m60h/keymaps/yukini/keymap.c
@@ -5,6 +5,7 @@
 
 #define KC_I1 MT(MOD_LGUI, KC_LNG1)
 #define KC_I2 MT(MOD_RGUI, KC_LNG2)
+#define SPC_L2 LT(2, KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
@@ -25,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(1),
-                          KC_LALT, KC_LGUI,                   KC_SPC,                             KC_RGUI, KC_RALT
+                          MO(2),   KC_LGUI,                   KC_SPC,                             KC_RGUI, KC_RALT
     ),
 
     [1] = LAYOUT_60_hhkb(
@@ -95,9 +96,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
         default:
             if (record->event.pressed) {
-                // 他キーが押下された場合、単キー押下扱いとしない
-                // space長押しでレイヤー移動したあとに、spaceキーを離した場合spaceのキーコードが送信されることを防ぐ
-                space_pressed = false;
+                // spaceを離す前に他キーを押下しても、spaceのキーコードを送信する
+                // vimのleader-keyにspaceを使っており、spaceキーが離れる前に他キーを押しがちなので設定
+                if (space_pressed) {
+                    register_code(KC_SPC);
+                    unregister_code(KC_SPC);
+                    space_pressed = false;
+                }
                 l_gui_pressed = false;
                 r_shift_pressed = false;
             }
